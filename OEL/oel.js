3ab583c50ef4f6fda7ddd4066c872310d79bff4e
basiljamil12 <118303446+basiljamil12@users.noreply.github.com>
2022-11-15 23:38:44 +0500

    Add files via upload

---
+++ OEL/oel.js
@@ -0,0 +1,43 @@
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function ListAdd() {
+    var selected = document.getElementById("torent").value;
+    var li = document.createElement("li");
+    var inputValue = document.getElementById(selected).innerHTML;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("print").appendChild(li);
+    document.getElementById(selected).value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("x");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
